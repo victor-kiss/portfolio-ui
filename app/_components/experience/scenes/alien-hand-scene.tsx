adc538224a95bf6c16e66d439f8d99ae94f13e77
victor kiss <victorkiss10@gmail.com>
2026-09-08 10:55:13 -0300

    fix(ui/ux): added new props  to orbitControls to use 2 fingers, fixing the broken page scroll

--- app/_components/experience/scenes/alien-hand-scene.tsx
+++ app/_components/experience/scenes/alien-hand-scene.tsx
@@ -6,40 +6,44 @@ import { Canvas } from "@react-three/fiber";
 import dynamic from "next/dynamic";
 import { Suspense } from "react";
 import ModelLoader from "../../ui/model-loader";
+import { TOUCH } from "three";
 
 const AlienHand = dynamic(() => import("../models/alien-hand"));
 export default function AlienHandScene() {
-  
   return (
     <>
       <Canvas
-      camera={{ position: [2, -0.5, 5], fov: 45 }}
-      gl={{ antialias: true }}
-      className="w-full h-full "
-    >
-      <ambientLight intensity={0.5} />
+        camera={{ position: [2, -0.5, 5], fov: 45 }}
+        gl={{ antialias: true }}
+        className="w-full h-full "
+      >
+        <ambientLight intensity={0.5} />
 
-      {/* O Center calcula os limites da malha (bounding box) e centraliza perfeitamente */}
-      <Center >
-        <Suspense fallback={
-          <Html center>
-            <ModelLoader/>
-          </Html>
-        }>
-             <AlienHand />
-        </Suspense>
-       
-      </Center>
-      <Environment preset="lobby" />
-      <OrbitControls
-        enableZoom={false}
-        autoRotate
-        autoRotateSpeed={2}
-        maxPolarAngle={Math.PI / 2}
-        minPolarAngle={Math.PI / 2 - 0.1}
-      />
-    </Canvas>
-
+        {/* O Center calcula os limites da malha (bounding box) e centraliza perfeitamente */}
+        <Center>
+          <Suspense
+            fallback={
+              <Html center>
+                <ModelLoader />
+              </Html>
+            }
+          >
+            <AlienHand />
+          </Suspense>
+        </Center>
+        <Environment preset="lobby" />
+        <OrbitControls
+          enableZoom={false}
+          autoRotate
+          autoRotateSpeed={2}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2 - 0.1}
+          touches={{
+            ONE: undefined,
+            TWO: TOUCH.ROTATE,
+          }}
+        />
+      </Canvas>
     </>
   );
 }
